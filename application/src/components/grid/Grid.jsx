9464a0e18c1ec2fab8786cd7f746a1f1cae10bf4
Sean McGeachie <sean@seans-air.home>
2021-10-12 01:40:28 +0100

    added start and finish nodes and tried to implement walls

--- application/src/components/grid/Grid.jsx
+++ application/src/components/grid/Grid.jsx
@@ -2,31 +2,42 @@ import React ,{useEffect, useState} from "react";
 import Node from "../node/Node.jsx";
 
 export default function Grid(){
-    const [nodes, setNodes] = useState([]);
+    const [nodesArr, setNodes] = useState([]);
     
     useEffect(() => {
         const nodes = [];
-        console.log("Component did mount");
-        for(let row=0; row < 15; row++){
+        for(let row=0; row < 20; row++){
             const currentRow = [];
             for(let col=0; col < 50; col++){
-                currentRow.push([]);
+                const currentNode = {
+                    col,
+                    row,
+                    isStart: row === 10 && col === 5,
+                    isFinish: row === 10 && col === 45,
+                };
+                currentRow.push(currentNode);
             }
             nodes.push(currentRow);
         }
         setNodes(nodes);
-        console.log(nodes);
     }, []);
 
+
     return(
-        <div className="grid m-24">
-            
-            {nodes.map((row, rowIdx) => {
-                return <div>
-                    {row.map((node, nodeIdx) => <Node></Node>)}
+        <div className="m-24 select-none">
+            {nodesArr.map((row, rowIdx) => {
+                return <div key={rowIdx}>
+                    {row.map((node, nodeIdx) => {
+                        const {isStart, isFinish} = node;
+                        return (
+                            <Node
+                                key={nodeIdx}
+                                isStart={isStart}
+                                isFinish={isFinish}></Node>
+                        );
+                    })}
                 </div>
             })}
         </div>
-
     );
 }
--- application/src/components/node/Node.jsx
+++ application/src/components/node/Node.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState}  from "react";
+
+export default function Node({isFinish, isStart}){
+    const [isWall, setIsWall] = useState(false);
+    const [isHover, setIsHover] = useState(false);
+    var mouseDown = false;
+    document.body.onmousedown = function() { 
+      mouseDown = true;
+      console.log(mouseDown);
+    }
+    document.body.onmouseup = function() {
+        mouseDown = false;
+        console.log(mouseDown); 
+    }     
 
 
-export default function Node(){
+
+    function test(){
+        if(mouseDown){
+            console.log("YE");
+            setIsWall(true);
+        }
+    }
 
     return(
-     <div className="node w-6 h-6 border border-gray-800 inline-block">
+     <div onMouseEnter={test} className={`w-6 h-6 border border-gray-800 inline-block ${isStart ? "bg-green-600" : ""} ${isFinish ? "bg-red-600" : ""} ${isWall ? "bg-blue-900" : ""}`}>
      </div>
     );
 }
