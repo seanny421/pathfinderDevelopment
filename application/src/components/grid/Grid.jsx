5fb0958d9c8c7854a1d49745e6a575b514d06003
Sean McGeachie SE2019 <sean.mcgeachie.2017@uni.strath.ac.uk>
2021-10-12 12:36:54 +0100

    got grid working and setting walls also work

--- application/src/components/grid/Grid.jsx
+++ application/src/components/grid/Grid.jsx
@@ -2,35 +2,86 @@ import React ,{useEffect, useState} from "react";
 import Node from "../node/Node.jsx";
 
 export default function Grid(){
-    const [nodesArr, setNodes] = useState([]);
+    const [grid, setGrid] = useState([]);
+    const [isMousePressed, setIsMousePressed] = useState(false);
     
     useEffect(() => {
-        const nodes = [];
+        setGrid(getGrid);
+    }, []);
+
+    function getGrid(){
+        const grid = [];
         for(let row=0; row < 20; row++){
             const currentRow = [];
             for(let col=0; col < 50; col++){
-                const currentNode = {
-                    col,
-                    row,
-                    isStart: row === 10 && col === 5,
-                    isFinish: row === 10 && col === 45,
-                };
-                currentRow.push(currentNode);
+                currentRow.push(createNode(col, row))
             }
-            nodes.push(currentRow);
+            grid.push(currentRow);
         }
-        setNodes(nodes);
-    }, []);
+        return grid;
+    }
+
+    function createNode(col, row){
+        return {
+            col,
+            row,
+            isStart: row === 10 && col === 5,
+            isFinish: row === 10 && col === 45,
+            isWall: false
+        };
+
+    }
+
+    function handleMouseDown(row, col){
+        const newGrid = newGridToggleWall(grid, row, col);
+        setGrid(newGrid);
+        setIsMousePressed(true);
+    }
+
+    function handleMouseEnter(row, col){
+        if(!isMousePressed) return;
+        const newGrid = newGridToggleWall(grid, row, col);
+        setGrid(newGrid);
+    }
+
+    function handleMouseUp(){
+        setIsMousePressed(false);
+    }
+
+    function newGridToggleWall(grid, row, col){
+        const newGrid = grid.slice();
+        const node = newGrid[row][col];
+        const newNode = {
+            ...node,
+            isWall: !node.isWall,
+        };
+        newGrid[row][col] = newNode;
+        return newGrid;
+
+    }
+
+    function refreshGrid(){
+        setGrid(getGrid);
+    }
 
 
     return(
         <div className="m-24 select-none">
-            {nodesArr.map((row, rowIdx) => {
-                return <div key={rowIdx}>
+            <button onClick={refreshGrid} className="p-2 bg-green-500 rounded text-white m-4">Visualize Algorithm</button>
+            <button onClick={refreshGrid} className="p-2 bg-green-500 rounded text-white m-4">Refresh</button>
+            {grid.map((row, rowIdx) => {
+                return <div key={rowIdx} className="font-none">
                     {row.map((node, nodeIdx) => {
-                        const {isStart, isFinish} = node;
+                        const {row, col, isWall, isStart, isFinish} = node;
                         return (
                             <Node
+                                isMousePressed={isMousePressed}
+                                onMouseDown={(row, col) => handleMouseDown(row, col)}
+                                onMouseEnter={(row, col) => handleMouseEnter(row,col)}
+                                onMouseUp={() => handleMouseUp()}
+                                isWall={isWall}
+                                row={row}
+                                col={col}
                                 key={nodeIdx}
                                 isStart={isStart}
                                 isFinish={isFinish}></Node>
--- application/src/components/node/Node.jsx
+++ application/src/components/node/Node.jsx
@@ -1,29 +1,13 @@
 import React, {useState}  from "react";
 
-export default function Node({isFinish, isStart}){
-    const [isWall, setIsWall] = useState(false);
-    const [isHover, setIsHover] = useState(false);
-    var mouseDown = false;
-    document.body.onmousedown = function() { 
-      mouseDown = true;
-      console.log(mouseDown);
-    }
-    document.body.onmouseup = function() {
-        mouseDown = false;
-        console.log(mouseDown); 
-    }     
-
-
-
-    function test(){
-        if(mouseDown){
-            console.log("YE");
-            setIsWall(true);
-        }
-    }
+export default function Node({row, col, isFinish, isStart, onMouseDown, isWall, onMouseUp, onMouseEnter}){
 
     return(
-     <div onMouseEnter={test} className={`w-6 h-6 border border-gray-800 inline-block ${isStart ? "bg-green-600" : ""} ${isFinish ? "bg-red-600" : ""} ${isWall ? "bg-blue-900" : ""}`}>
+     <div 
+        onMouseDown={() => onMouseDown(row, col)} 
+        onMouseUp={() => onMouseUp()} 
+        onMouseEnter={() => onMouseEnter(row, col)} 
+        className={` node  ${isStart ? "bg-green-600" : ""} ${isFinish ? "bg-red-600" : ""} ${isWall ? "bg-blue-900" : ""}`}>
      </div>
     );
 }
